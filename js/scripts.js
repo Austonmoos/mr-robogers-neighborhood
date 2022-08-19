affaa4c7704dd4fa8df8fbeae19a87e0e5a95ff9
Auston Moos <AustonHMoos2004@gmail.com>
2022-08-19 14:00:38 -0700

    add code to JAVASCRIPT

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,28 @@ let retVal;
 let wordToArray = word.split('');
     for(let i = 0; i <=1; i += 1) {
       if (wordToArray{1} === 1[i]) {
-        retVal = word + ""
+        retVal = word + "Beep!"
       }
     }
 }
+
+function mr-robogers-neighborhood(word) {
+  let retVal;
+  let wordToArray = word.split('');
+      for(let i = 0; i <=1; i += 1) {
+        if (wordToArray{2} === 2[i]) {
+          retVal = word + "boop!"
+        }
+      }
+    }
+
+    function mr-robogers-neighborhood(word) {
+      let retVal;
+      let wordToArray = word.split('');
+          for(let i = 0; i <=1; i += 1) {
+            if (wordToArray{3} === 3[i]) {
+              retVal = word + "Won't you be my neighbor?"
+            }
+          }
+      }
+      
